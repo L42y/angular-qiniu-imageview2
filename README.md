35afa8ac4e31080b9ec8aed64b60d51895341e83
L42y <423300@gmail.com>
2014-03-18 05:58:55 +0200

    init: initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+# [angular](http://angularjs.org)-[qiniu](http://www.qiniu.com)-[imageview2](http://developer.qiniu.com/docs/v6/api/reference/fop/image/imageview2.html)
+
+AngularJS component for preprocessing image URL from qiniu.com(七牛云存储)
---
+++ angular-qiniu-imageview2.js
@@ -0,0 +1,26 @@
+angular.module('l42y.qiniu.imageview2', [
+]).directive('qiniuImageview2', ['$window', function (
+  $window
+) {
+  var pixelRatio = $window.getDevicePixelRatio();
+
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attrs) {
+      var mode = $attrs.qiniuImageview2Mode;
+      var format = $attrs.qiniuImageview2Format;
+      var formatString = format ? '/format/' + format : '';
+      var quality = $attrs.qiniuImageview2Quality;
+      var qualityString = quality ? '/quality/' + quality : '';
+
+      $attrs.$observe('qiniuImageview2', function (src) {
+        var imageSrc = src + '?imageView2/' + mode +
+              '/w/' + $attrs.width * pixelRatio +
+              '/h/' + $attrs.height * pixelRatio +
+              qualityString + formatString;
+
+        $attrs.$set('src', imageSrc);
+      });
+    }
+  };
+}]);
