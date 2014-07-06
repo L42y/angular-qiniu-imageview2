ea8f427abae3c12aee50e4f13fc2867b99266bda
L42y <423300@gmail.com>
2014-07-07 01:23:58 +0800

    feat: interlace parameter

--- angular-qiniu-imageview2.js
+++ angular-qiniu-imageview2.js
@@ -10,16 +10,18 @@ angular.module('l42y.qiniu.imageview2', [
       var mode = $attrs.qiniuImageview2Mode;
       var format = $attrs.qiniuImageview2Format;
       var quality = $attrs.qiniuImageview2Quality;
+      var interlace = $attrs.qiniuImageview2Interlace;
 
       var formatStr = format ? '/format/' + format : '';
       var qualityStr = quality ? '/quality/' + quality : '';
+      var interlaceStr = interlace ? '/interlace/1' : '';
 
       $attrs.$observe('qiniuImageview2', function (src) {
         if (src.length) {
           var imageSrc = src + '?imageView2/' + mode +
                 '/w/' + $attrs.width * pixelRatio +
                 '/h/' + $attrs.height * pixelRatio +
-                qualityStr + formatStr;
+                qualityStr + formatStr + interlaceStr;
 
           $attrs.$set('src', imageSrc);
         }
