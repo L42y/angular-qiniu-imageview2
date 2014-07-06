7552567799e7cb8b3fc7af12f0ed772b02599b8d
L42y <423300@gmail.com>
2014-07-07 01:22:25 +0800

    refactor: naming & ordering

--- angular-qiniu-imageview2.js
+++ angular-qiniu-imageview2.js
@@ -9,16 +9,17 @@ angular.module('l42y.qiniu.imageview2', [
     link: function ($scope, $element, $attrs) {
       var mode = $attrs.qiniuImageview2Mode;
       var format = $attrs.qiniuImageview2Format;
-      var formatString = format ? '/format/' + format : '';
       var quality = $attrs.qiniuImageview2Quality;
-      var qualityString = quality ? '/quality/' + quality : '';
+
+      var formatStr = format ? '/format/' + format : '';
+      var qualityStr = quality ? '/quality/' + quality : '';
 
       $attrs.$observe('qiniuImageview2', function (src) {
         if (src.length) {
           var imageSrc = src + '?imageView2/' + mode +
                 '/w/' + $attrs.width * pixelRatio +
                 '/h/' + $attrs.height * pixelRatio +
-                qualityString + formatString;
+                qualityStr + formatStr;
 
           $attrs.$set('src', imageSrc);
         }
