3dd9fbf0713dada6c3049c625695419e6c9d88b3
L42y <423300@gmail.com>
2014-08-08 06:31:40 +0800

    refactor: depends on angular-qiniu-image

--- angular-qiniu-imageview2.js
+++ angular-qiniu-imageview2.js
@@ -1,12 +1,14 @@
 angular.module('l42y.qiniu.imageview2', [
+  'l42y.qiniu.image'
 ]).directive('qiniuImageview2', ['$window', function (
   $window
 ) {
   var pixelRatio = $window.getDevicePixelRatio().toFixed();
 
   return {
+    require: '^qiniuImage',
     restrict: 'A',
-    link: function ($scope, $element, $attrs) {
+    link: function ($scope, $element, $attrs, qiniu) {
       var mode = $attrs.qiniuImageview2Mode;
       var format = $attrs.qiniuImageview2Format;
       var quality = $attrs.qiniuImageview2Quality;
@@ -16,16 +18,11 @@ angular.module('l42y.qiniu.imageview2', [
       var qualityStr = quality ? '/quality/' + quality : '';
       var interlaceStr = interlace ? '/interlace/1' : '';
 
-      $attrs.$observe('qiniuImageview2', function (src) {
-        if (src.length) {
-          var imageSrc = src + '?imageView2/' + mode +
-                '/w/' + $attrs.width * pixelRatio +
-                '/h/' + $attrs.height * pixelRatio +
-                qualityStr + formatStr + interlaceStr;
-
-          $attrs.$set('src', imageSrc);
-        }
-      });
+      var fop = 'imageView2/' + mode +
+            '/w/' + $attrs.width * pixelRatio +
+            '/h/' + $attrs.height * pixelRatio +
+            qualityStr + formatStr + interlaceStr;
+      qiniu.fops.push(fop);
     }
   };
 }]);
